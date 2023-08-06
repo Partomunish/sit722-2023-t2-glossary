f6506ac272a6f62a5da910747ddbf1347b9e515c
Patrick Kanyugi <partomunish@gmail.com>
2023-08-06 11:47:37 +0300

    For Task 3.2P

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
 app.set('view engine', 'ejs');
 
-app.use((request, response, next) => {
-    // response.status(200).json(
-    //     {
-    //         message: "It works!"
-    //     }
-    // );
-
+app.get("/", (request, response, next) => {
     const terms = [
         {
             Term: "Term 1",
@@ -20,7 +15,35 @@ app.use((request, response, next) => {
             ]
         }
     ]
+
     response.render("index");
+});
+
+app.get("/glossary-items", (request, response) => {
+    let filename = "C:\\Users\\USER\\Documents\\T2\\sit722-2023-t2-glossary\\views\\somefile.txt"
+    fs.readFile(filename, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        let splitData = data.split("\r\n");
+        let glossaryItems = [];
+        let glossaryItem;
+        for (let i=0; i<splitData.length; i++) {
+            let currentItem = splitData[i];
+
+            if (currentItem === "" || i === splitData.length - 1) {
+                if (glossaryItem) {
+                    glossaryItems.push(glossaryItem);
+                }
+                glossaryItem = [];
+            } else {
+                glossaryItem.push(currentItem);
+            }
+        }
+
+        response.send(glossaryItems);
+    });
 });
 
 module.exports = app;
